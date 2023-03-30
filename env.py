bedeee32554ec05d62d29551759f20ea6162f50b
Heattehnik <gadjka@mail.ru>
2023-03-30 09:34:09 +0500

    Рефакторинг

--- env.py
+++ env.py
@@ -1,3 +1,4 @@
-DATA_BASE = '../processing.db'
-BOT_TOKEN = '5673270666:AAGt6jdQGncbShP3bDyiXTu4ZXeBm6ykSuE'
+DATA_BASE = '../database/processing.db'
+BOT_TOKEN = '6108527782:AAFLlaF8M0yPzsAPdECqyR_GbdwnaM-J8Ac'
 # '6108527782:AAFLlaF8M0yPzsAPdECqyR_GbdwnaM-J8Ac' боевой бот
+# 5673270666:AAGt6jdQGncbShP3bDyiXTu4ZXeBm6ykSuE тестовый бот
